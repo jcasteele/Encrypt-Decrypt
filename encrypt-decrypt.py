62bf0ae4462df77626252091a490f04632602ab9
jcasteele <john.casteele@outlook.com>
2023-03-31 18:35:28 -0500

    Create encrypt-decrypt.py
    Initial commit of encrypt-decrypt

---
+++ encrypt-decrypt.py
@@ -0,0 +1,13 @@
+import time
+import os
+import sys
+import transpositionEncrypt
+import transpositionDecrypt
+
+USERFILE = "kittenhugger.txt"
+
+def main():
+    filename = USERFILE
+
+if __name__ == "__main__":
+    main()
